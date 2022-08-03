8a7808b8d794a5247c8abdac7ea3058512602455
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-08-03 15:31:02 +0700

    Q1.1-Q1.6

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,28 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit here</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'resa', 'event'],
+  inject: ['GStore'], //<---- Inject the Global Store
+  methods: {
+    edit() {
+      //Assuming successful API call to register them
+
+      //Set a flash message to appear on the next page loaded which says
+      //'You are successfully registered for ' + this.event.title
+      this.GStore.flashMessage = 'The data has been update '
+      setTimeout(() => {
+        //After 3 secound remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
